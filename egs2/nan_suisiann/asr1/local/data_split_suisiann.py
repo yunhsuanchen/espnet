0125f172d285f3a13e0e7d3cffabc2dcc73b2413
Ubuntu <ubuntu@ip-172-31-15-167.us-east-2.compute.internal>
2022-04-24 04:00:52 +0000

    refactor

--- egs2/nan_suisiann/asr1/local/data_split_suisiann.py
+++ egs2/nan_suisiann/asr1/local/data_split_suisiann.py
@@ -5,61 +5,11 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+import argparse
 
 from speech_augmentation import create_augment_wav_files
 
-train_size = 0.9
-dev_size = 0.05
-random_state = 11737
-
-max_audio_length = 20
-
-data_dir = sys.argv[1]  # downloads/0.2.1
-output_text = sys.argv[2] # tailo or cmn
-speech_aug = bool(sys.argv[3])
-spk_id = "spk001"
-
-if output_text == "tailo":
-    text_column_name = "羅馬字"
-elif output_text == "cmn":
-    text_column_name = "漢字"
-else:
-    raise NotImplementedError
-
-df = pd.read_csv(os.path.join(data_dir, "SuiSiann.csv"))
-
-train_df, dev_df, test_df = \
-              np.split(df.sample(frac=1, random_state=random_state), 
-                       [int(train_size*len(df)), 
-                       int((train_size+dev_size)*len(df))])
-
-if speech_aug:
-    aug_file = os.path.join(data_dir, "SuiSiann_aug.csv")
-
-    if os.path.exists(aug_file):
-        print("data augmentation files already exist, reading from csv file:", aug_file)
-        aug_train_df = pd.read_csv(aug_file)
-
-    else:
-        pitch_factors = [-1, -2, -3]
-        aug_wav_dir = os.path.join(data_dir, "augmented")
-
-        if not os.path.isdir(aug_wav_dir):
-            print(f"Create directory:{aug_wav_dir}")
-            os.makedirs(aug_wav_dir)
-
-        aug_train_df = create_augment_wav_files(train_df, pitch_factors, data_dir, aug_wav_dir, spk_id=spk_id)
-
-        # save df to csv
-        aug_train_df.to_csv(aug_file, index=False)
-        print(f"Finished data augmentation, augmented wav files saved at {aug_wav_dir}")
-        print("Data augmentation csv saved at", aug_file)
-
-print(
-    f"# train: {len(train_df)}, # dev:{len(dev_df)}, # test:{len(test_df)}"
-)
-
-def create_files(df, directory, train=False):
+def create_files(args, df, directory, train=False):
     text_lines, scp_lines, utt2spk_lines = [], [], []
     human_annotation_lines = []
 
@@ -68,7 +18,7 @@ def create_files(df, directory, train=False):
         if row["長短"] > max_audio_length:
             continue
 
-        if train and speech_aug:
+        if train and args.speech_aug:
             spk_id = row["講者"]
         else:
             # suisiann only has one speaker
@@ -109,18 +59,87 @@ def create_files(df, directory, train=False):
     with open(f"{directory}/human_annotation.csv", "w+") as f:
         f.writelines(human_annotation_lines)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', default='downloads/0.2.1',
+                        help='path to SuiSiann data directory')
+    parser.add_argument('--output_text', default='tailo',
+                        choices = ["tailo", "cmn"],
+                        help='ASR outout script')
+    parser.add_argument('--speech_aug', action='store_true',
+                        help="add data augmentation: pitch shift")
+    parser.add_argument('--pseudo_label', action='store_true',
+                        help="read from MT output as label")                   
 
-print("Creating files for train...", end="")
-if speech_aug:
-    create_files(aug_train_df, "data/train", train=True)
-else:
-    create_files(train_df, "data/train", train=True)
-print("Done.")
+    args = parser.parse_args()
 
-print("Creating files for dev...", end="")
-create_files(dev_df, "data/dev")
-print("Done.")
+    train_size = 0.9
+    dev_size = 0.05
+    random_state = 11737
 
-print("Creating files for test...", end="")
-create_files(test_df, "data/test")
-print("Done.")
+    max_audio_length = 20
+
+    # data_dir = sys.argv[1]  # downloads/0.2.1
+    # output_text = sys.argv[2] # tailo or cmn
+    # speech_aug = bool(sys.argv[3])
+
+    data_dir = args.data_dir
+    output_text = args.output_text
+
+
+    spk_id = "spk001"
+
+    if output_text == "tailo":
+        text_column_name = "羅馬字"
+    elif output_text == "cmn":
+        text_column_name = "漢字"
+    else:
+        raise NotImplementedError
+
+    df = pd.read_csv(os.path.join(data_dir, "SuiSiann.csv"))
+
+    train_df, dev_df, test_df = \
+                np.split(df.sample(frac=1, random_state=random_state), 
+                        [int(train_size*len(df)), 
+                        int((train_size+dev_size)*len(df))])
+
+    if args.speech_aug:
+        aug_file = os.path.join(data_dir, "SuiSiann_aug.csv")
+
+        if os.path.exists(aug_file):
+            print("data augmentation files already exist, reading from csv file:", aug_file)
+            aug_train_df = pd.read_csv(aug_file)
+
+        else:
+            pitch_factors = [-1, -2, -3]
+            aug_wav_dir = os.path.join(data_dir, "augmented")
+
+            if not os.path.isdir(aug_wav_dir):
+                print(f"Create directory:{aug_wav_dir}")
+                os.makedirs(aug_wav_dir)
+
+            aug_train_df = create_augment_wav_files(train_df, pitch_factors, data_dir, aug_wav_dir, spk_id=spk_id)
+
+            # save df to csv
+            aug_train_df.to_csv(aug_file, index=False)
+            print(f"Finished data augmentation, augmented wav files saved at {aug_wav_dir}")
+            print("Data augmentation csv saved at", aug_file)
+
+    print(
+        f"# train: {len(train_df)}, # dev:{len(dev_df)}, # test:{len(test_df)}"
+    )
+
+    print("Creating files for train...", end="")
+    if args.speech_aug:
+        create_files(args, aug_train_df, "data/train", train=True)
+    else:
+        create_files(args, train_df, "data/train", train=True)
+    print("Done.")
+
+    print("Creating files for dev...", end="")
+    create_files(args, dev_df, "data/dev")
+    print("Done.")
+
+    print("Creating files for test...", end="")
+    create_files(args, test_df, "data/test")
+    print("Done.")
